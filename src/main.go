7b33b7986748c76fa54a9a0cf9f006fbeae3e1b5
Scott Williams <slwilliams@cantab.net>
2020-01-14 22:10:55 +0000

    Setup golang webserver

--- src/main.go
+++ src/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+  "log"
+  "net/http"
+)
 
+func main() {
+  fs := http.FileServer(http.Dir("assets"))
+  http.Handle("/assets", fs)
+  http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "index.html")
+  })
+
+  log.Println("Listening...")
+  http.ListenAndServe(":1337", nil)
 }
