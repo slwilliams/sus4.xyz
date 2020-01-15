664867e2e14a6103859af07cd0227c7a743b36e4
Scott Williams <slwilliams@cantab.net>
2020-01-15 21:34:58 +0000

    Factor out JS into a separate file, had to make the golang server set the correct MIME type to be able to use type module...

--- src/main.go
+++ src/main.go
@@ -3,11 +3,16 @@ package main
 import (
   "log"
   "net/http"
+  "mime"
+  "path/filepath"
 )
 
 func main() {
   fs := http.FileServer(http.Dir("assets"))
-  http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+  http.HandleFunc("/assets/",  func (w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", mime.TypeByExtension(filepath.Ext(r.RequestURI)))
+    http.StripPrefix("/assets/", fs).ServeHTTP(w, r)
+  })
   http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w, r, "index.html")
   })
